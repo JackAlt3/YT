b52f3563cf66f3a44dc75d30e8c144fa57a9fc43
JackAlt3 <86922808+JackAlt3@users.noreply.github.com>
2024-03-30 13:48:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,18 +3,10 @@
 from youtube_transcript_api import YouTubeTranscriptApi
 import os
 # put in the "" the final part of the video in the address bar of the browser
-#srt = YouTubeTranscriptApi.get_transcript("Yy1yx9uTHIs")
-video_id = "ce5tWoPPRIQ"
-#transcript_list = YouTubeTranscriptApi.list_transcripts("Yy1yx9uTHIs")
-transcript = YouTubeTranscriptApi.get_transcript("Yy1yx9uTHIs", languages=['en'])
-#transcript = transcript_list.find_transcript(['de', 'en','hi'])
-print(transcript)
-#print(srt)
-'''
+transcript = YouTubeTranscriptApi.get_transcript("Yy1yx9uTHIs", languages=['hi']) #put yr yt code and language
+
 text = ""
-with open("file.txt", "w") as file:
-    for i in srt:
-        text += i["text"] + "..."
-    file.write(text)
-os.startfile("file.txt")
-'''
+
+for i in transcript:
+    text += i["text"] + "..."
+print(text)
